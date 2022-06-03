177f656de2d682b549518922131440e28d4fbdf8
agafonova14011985 <agafonova14011985@mail.ru>
2022-06-04 00:31:17 +0700

    Home_Work_4

--- common/src/main/java/transport/NioTransportChannel.java
+++ common/src/main/java/transport/NioTransportChannel.java
@@ -1,48 +1,118 @@
 package transport;
 
+import logging.CommonLogger;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import messages.IMessage;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.List;
 
 
+//@Interceptors(CommonLogger.class)
 public class NioTransportChannel implements ITransportChannel
 {
     private static final int READ_BUFFER_CAPACITY = 256;
 
+    @Getter
+    @Setter
+    private SocketChannel socketChannel;
 
     @Override
-    public void sendMessage(@NotNull IMessage message) {
+    @SneakyThrows
+    public synchronized void sendMessage(@NotNull IMessage message) {
 
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(message);
+        objectOutputStream.close();
+        final ByteBuffer sourceBuffer = ByteBuffer.wrap(byteArrayOutputStream.toByteArray());
+        socketChannel.write(sourceBuffer);
     }
 
+
+
+    @NotNull
     @Override
-    public @NotNull IMessage readMessage() throws CorruptedDataReceived, RemoteEndGone {
-        return null;
+    @SneakyThrows
+    public synchronized IMessage readMessage()
+    {
+        final ByteBuffer nioBuffer = ByteBuffer.allocate(READ_BUFFER_CAPACITY);
+        final List<Byte> accumulator = new ArrayList<>();
+        int read;
+        while ((read = socketChannel.read(nioBuffer)) > 0)
+        {
+            nioBuffer.flip();
+            final byte[] temporaryBuffer = new byte[nioBuffer.limit()];
+            nioBuffer.get(temporaryBuffer);
+            for (final byte b : temporaryBuffer) accumulator.add(b);
+            nioBuffer.clear();
+        }
+
+        if (read < 0)
+        {
+            socketChannel.close();
+            throw new RemoteEndGone();
+        }
+
+        final byte[] temporaryBuffer = new byte[accumulator.size()];
+        int i = 0;
+        for (final Byte b : accumulator) temporaryBuffer[i++] = b;
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(temporaryBuffer);
+        final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+
+        try
+        {
+            final Object receivedObject = objectInputStream.readObject();
+            if (receivedObject instanceof IMessage)
+            {
+                return (IMessage) receivedObject;
+            }
+        }
+        catch (Exception e)
+        {
+            throw new CorruptedDataReceived();
+        }
+
+        throw new CorruptedDataReceived();
     }
+
+
 
     @Override
     public void closeSilently() {
 
+        try
+        {
+            close();
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 
     @Override
     public String getRemoteAddress() {
-        return null;
+        if (!isConnected()) return null;
+        return socketChannel.socket().getInetAddress().toString();
     }
 
     @Override
     public boolean isConnected() {
-        return false;
+        if (socketChannel == null) return false;
+        return socketChannel.isConnected();
     }
 
     @Override
     public void close() throws IOException {
 
+        if (socketChannel != null)
+            socketChannel.close();
     }
 }
